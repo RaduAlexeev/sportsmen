0a9838c202ecb6c6ac4f0f9d65b48835e33f37f2
Radu Alexeev <116222934+RaduAlexeev@users.noreply.github.com>
2023-05-08 13:00:35 +0300

    add new style

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "mainWidth": "1366px"
+      },
+      colors: {
+        "mainRed": "#e4282c",
+        "burgundy": "#be1f23"
+      },
+      fontSize: {
+        "fontHeader": "27px",
+        "fontMenu": "22px"
+      },
+      boxShadow: {
+        "shadow-x-black": "-10px 0 20px -5px black, 10px 0 20px -5px black",
+        "shadow-line": "2px 0px 5px black"
+      },
+      skew: {
+        "30": "30deg"
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
