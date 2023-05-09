8a719068c63a1447ee762afaa2aa5aa6cab0b23b
Radu Alexeev <116222934+RaduAlexeev@users.noreply.github.com>
2023-05-09 14:41:15 +0300

    add FooterMenu, Map, and FooterInfo components

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,9 +1,15 @@
+import FooterMenu from "./footerMenu";
+import Map from "./map";
 import FooterInfo from "./footerInfo";
 
 export default function Footer() {
     return (
         <div className="bg-forFooter w-full h-footer mt-28 flex justify-center">
-            <div className="w-mainWidth h-full bg-slate-500">
+            <div className="w-mainWidth h-full flex flex-col justify-around text-white">
+                <div className="flex flex-row tracking-tight">
+                    <FooterMenu />
+                    <Map />
+                </div>
                 <FooterInfo />
             </div>
         </div>
