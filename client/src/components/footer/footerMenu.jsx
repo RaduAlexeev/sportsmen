cce22b7a299b401ca249ecfda6d454b5029f50d8
Radu Alexeev <116222934+RaduAlexeev@users.noreply.github.com>
2023-05-09 14:40:11 +0300

    add menu

---
+++ client/src/components/footer/footerMenu.jsx
@@ -0,0 +1,23 @@
+export default function FooterMenu() {
+    return (
+        <div className="w-1/2 flex flex-row justify-around items-center leading-10 text-base font-normal">
+
+            <ul>
+                <li><a href="#">Acasa</a></li>
+                <li><a href="#">Agenda sportiva</a></li>
+                <li><a href="#">Stiri</a></li>
+                <li><a href="#">Politica de colaborare</a></li>
+                <li><a href="#">All sports</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="#">Top sportsmen</a></li>
+                <li><a href="#">Top NEWS</a></li>
+                <li><a href="#">Contul meu</a></li>
+                <li><a href="#">Despre noi</a></li>
+                <li><a href="#">Contacte</a></li>
+            </ul>
+
+        </div>
+    );
+}
