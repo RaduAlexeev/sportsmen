098ca883d5b34778930a27358faca422589f05f1
Radu Alexeev <116222934+RaduAlexeev@users.noreply.github.com>
2023-05-09 14:40:32 +0300

    add info

--- client/src/components/footer/footerInfo.jsx
+++ client/src/components/footer/footerInfo.jsx
@@ -1,31 +1,5 @@
-export default function FooterInfo() {
+export default function footerInfo() {
     return (
-        <div className="flex flex-row justify-around items-center leading-10">
-
-            <ul>
-                <li>Acasa</li>
-                <li>Agenda sportiva</li>
-                <li>Stiri</li>
-                <li>Politica de colaborare</li>
-                <li>All sports</li>
-            </ul>
-
-            <ul>
-                <li>Top sportsmen</li>
-                <li>Top NEWS</li>
-                <li>Contul meu</li>
-                <li>Despre noi</li>
-                <li>Contacte</li>
-            </ul>
-
-            <ul>
-                <li>Acasa</li>
-                <li>Agenda sportiva</li>
-                <li>Stiri</li>
-                <li>Politica de colaborare</li>
-                <li>All sports</li>
-            </ul>
-
-        </div>
+        <p className="text-center">© www.sportsmen.md | Toate drepturile sunt rezervate</p>
     );
 }
