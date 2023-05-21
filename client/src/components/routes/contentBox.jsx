b39afc8ecd36f5bce2476dc46982bfd2099d0a11
Radu Alexeev <116222934+RaduAlexeev@users.noreply.github.com>
2023-05-22 02:00:17 +0300

    add content box component

---
+++ client/src/components/routes/contentBox.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+
+export default function ContentBox({ children }) {
+    ContentBox.propTypes = {
+        children: PropTypes.string.isRequired
+    };
+
+    return (
+        <div className="w-full h-[1400px] flex justify-center">
+            <div className="w-mainWidth flex flex-row justify-between items-center bg-violet-900">
+                {children}
+            </div>
+        </div>
+    );
+}
